133861e47c08ad30fa7210dbeac629f48633375e
carla17preda <carla_preda@yahoo.com>
2023-02-15 14:54:22 +0200

    login

---
+++ login/login_demo.py
@@ -0,0 +1,11 @@
+import logging
+
+
+logging.basicConfig(filename="test.log",level=logging.DEBUG) #creeaza fisier
+
+
+logging.warning("waring message")
+logging.info("info message")
+logging.error("error_message")
+
+
