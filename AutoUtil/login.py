81e663fb35dba4c06b6cb0688dd7941a86163f3b
carla17preda <carla_preda@yahoo.com>
2023-03-02 14:03:57 +0200

    proiectBun

--- AutoUtil/login.py
+++ AutoUtil/login.py
@@ -3,9 +3,10 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 import time
 
-def login():
+
+def login(driver="",email="",passw=""):
     baseUrl = "https://autoutil.thedemo.is/"
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
     driver.get(baseUrl)
     driver.maximize_window()
     driver.implicitly_wait(15)
@@ -16,14 +17,13 @@ def login():
     intra_in_cont.click()
     time.sleep(1)
 
-    email = driver.find_element(By.XPATH, "//input[@type='text']")
-    email.send_keys("carla_preda@yahoo.com")
+    email_input = driver.find_element(By.XPATH, "//input[@type='text']")
+    email_input.send_keys(email)
 
     time.sleep(0.5)
-    parola = driver.find_element(By.XPATH, "/html//div[@id='__next']//div[3]/div[2]/input")
-    parola.send_keys("Testare123@")
+    parola_input = driver.find_element(By.XPATH, "/html//div[@id='__next']//div[3]/div[2]/input")
+    parola_input.send_keys(passw)
 
     intra_in_cont2 = driver.find_element(By.XPATH, "//div[@id='__next']//div[.='INTRĂ ÎN CONT']")
     intra_in_cont2.click()
     time.sleep(1)
-
