b147d385ff5295f9f0c8845843eb8bbf5a43ffc9
carla17preda <carla_preda@yahoo.com>
2023-03-02 14:05:31 +0200

    proiectBun

--- AutoUtil/adauga_masina.py
+++ AutoUtil/adauga_masina.py
@@ -1,31 +1,29 @@
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 import time
 from AutoUtil.login import login
 
 
-def adauga():
+def adauga_masisna(driver="",nr_masina=""):
+    email = "carla_preda@yahoo.com"
+    passw = "Testare123@"
+    baseUrl = "https://autoutil.thedemo.is/"
+    driver.get(baseUrl)
+    driver.maximize_window()
+    driver.implicitly_wait(15)
+    time.sleep(3)
 
-        baseUrl = "https://autoutil.thedemo.is/acasa"
-        driver = webdriver.Chrome()
-        driver.get(baseUrl)
-        driver.maximize_window()
-        driver.implicitly_wait(15)
-        time.sleep(3)
-        login()
+    login(driver, email, passw)
 
-        # adauga masina
-        adauga = driver.find_element(By.XPATH, "/html//div[@id='__next']//div[@role='button']")
-        adauga.click()
-        time.sleep(1)
+    # adauga masina
+    adauga = driver.find_element(By.XPATH, "/html//div[@id='__next']//div[@role='button']")
+    adauga.click()
+    time.sleep(1)
 
-        nr_inmatriculare = driver.find_element(By.XPATH, "/html//div[@id='__next']//input")
-        nr_inmatriculare.send_keys("VN 06 HRK")
-        time.sleep(1)
+    nr_inmatriculare = driver.find_element(By.XPATH, "/html//div[@id='__next']//input")
+    nr_inmatriculare.send_keys(nr_masina)
+    time.sleep(1)
 
-        adauga_masina = driver.find_element(By.XPATH, "/html//div[@id='__next']//div[.='ADAUGĂ MAȘINĂ']")
-        adauga_masina.click()
-        time.sleep(1)
-
-adauga()
+    adauga_masina = driver.find_element(By.XPATH, "/html//div[@id='__next']//div[.='ADAUGĂ MAȘINĂ']")
+    adauga_masina.click()
+    time.sleep(1)
